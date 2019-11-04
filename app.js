edf34eb8ba736bf496448c7a92c61b1fde64037e
Steve Gossip <stephengossip@gmail.com>
2019-11-04 18:29:59 +0000

    added a Book model schema using mongoose and have required in app.js. after inserting books into this collection I use app.get to create a route to /books which shows the books in json format. All working.

--- app.js
+++ app.js
@@ -2,8 +2,33 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
+var Book = require('./Book.model.js');
 var port = 8080;
+
+var db = 'mongodb://localhost/example';
+
+mongoose.connect(db);
+
+app.get("/", function(req,res) {
+  res.send("happy to be here");
+});
+
+app.get("/books", function(req,res) {
+  console.log("getting all the books");
+  Book.find({
+
+  })
+  .exec(function(err,books){
+    if(err) {
+      res.send('error has occured')
+    }
+    else{
+      console.log(books);
+      res.json(books);
+    }
+  })
+});
+
 
 app.listen(port, function() {
   console.log('app listening on port ' + port);
